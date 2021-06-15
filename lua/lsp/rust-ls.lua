74c5d7922399d34dba943966a9c6c5f906a7a220
dank <35282038+d-ank@users.noreply.github.com>
2021-06-14 22:36:55 -0700

    fix RLS not running build scripts

--- lua/lsp/rust-ls.lua
+++ lua/lsp/rust-ls.lua
@@ -1,5 +1,16 @@
 require'lspconfig'.rust_analyzer.setup{
     cmd = {DATA_PATH .. "/lspinstall/rust/rust-analyzer"},
-    on_attach = require'lsp'.common_on_attach
+    on_attach = require'lsp'.common_on_attach,
+    settings = {
+        rust_analyzer = {
+            cargo = {
+                loadOutDirsFromCheck = true;
+                runBuildScripts = true;
+            },
+            procMacros = {
+                enable = true;
+            }
+        }
+    }
 }
 
